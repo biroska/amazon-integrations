73acb8d920dcae85447de7c26773370116dad61c
Aimberê Galdino <biroska@gmail.com>
2025-03-07 00:00:23 -0300

    Configuracao para uso do localstack com SQS

--- src/main/java/br/com/biroska/amazonintegrations/integration/aws/sqs/SqsConfig.java
+++ src/main/java/br/com/biroska/amazonintegrations/integration/aws/sqs/SqsConfig.java
@@ -2,11 +2,14 @@ package br.com.biroska.amazonintegrations.integration.aws.sqs;
 
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.net.URI;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,9 +17,13 @@ public class SqsConfig {
 
     private final StaticCredentialsProvider awsCredentialsProvider;
 
+    @Value("${aws.domain.sqs}")
+    private String sqsDomain;
+
     @Bean
     public SqsAsyncClient sqsAsyncClient(){
         return SqsAsyncClient.builder()
+                .endpointOverride(URI.create( sqsDomain ))
                 .region(Region.US_EAST_1 )
                 .credentialsProvider( awsCredentialsProvider )
                 .build();
